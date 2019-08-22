8b464ffab5f382f82dea3bc1059d7dd8111b112c
Omran Losinno <olosinno@comcast.net>
2019-08-22 10:35:42 -0400

    Removed Misc folder and all its contents for migration to new repository.

---
+++ AbsoluteValue_Test/AbsoluteValue_Test.java
@@ -0,0 +1,10 @@
+import java.math.*;
+
+public class AbsoluteValue_Test {
+    public static void main(String[] args) {
+
+        int value = -1;
+
+        System.out.println("The absolute value of " + value + " is " + Math.abs(value));
+    }
+}
--- Assessment_1/Runtime_Polymorphism.java
+++
@@ -1,9 +0,0 @@
-public class Runtime_Polymorphism {
-    public static void main(String[] args) {
-        int value = 1;
-        String string = "one";
-
-        string = string.valueOf(value);
-        System.out.println(string);
-    }
-}
