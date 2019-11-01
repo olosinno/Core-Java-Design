4a87a60ad3258d5c215d4e4487b5a75367c41b12
Omran Losinno <olosinno@comcast.net>
2019-11-01 10:17:20 -0400

    Integrated uses of both RemoveAll and Stream API

--- Array_List/Array_List.java
+++ Array_List/Array_List.java
@@ -1,10 +1,12 @@
 import java.util.ArrayList; // import the ArrayList class from java.util package
 import java.util.Collections;
+import java.util.List; // Collection interface, import for dynamic shift between List subtypes
 import java.util.Random; // import the Random class
+import java.util.stream.Collectors;
 
 public class Array_List {
     public static void main(String[] args) {
-        ArrayList<Integer> grades = new ArrayList<Integer>(); // create ArrayList named grades that stores integers
+        List<Integer> grades = new ArrayList<Integer>(); // create ArrayList from List named grades that stores integers
 
         grades.add(110); // add element to ArrayList
         System.out.println("Lil' Billy scored a perfect on the test with a " + grades.get(0) + "%!  A++");
@@ -23,13 +25,15 @@ public class Array_List {
         grades.add(randNo.nextInt(90) + 10);
         grades.add(randNo.nextInt(90) + 10);
         grades.add(randNo.nextInt(90) + 10); // add fifth object to ArrayList Grades
+        grades.add(90);
+        grades.add(90);
 
         /* Use a do while loop to add 45 extra grade numbers to the ArrayList instead of re-typing line a bunch */
         int group_size = 1;
         do {
             grades.add(randNo.nextInt(90) + 10);
             group_size++;
-        } while (group_size < 45);
+        } while (group_size < 43);
 
         /* Create for loop to iterate through ArrayList and print all added random grade integers;
         size() method retrieves size of ArrayList i.e. how many elements are inside
@@ -47,6 +51,17 @@ public class Array_List {
         type as base type of array/ArrayList, followed by a colon, which is then followed by the array name.
         Is more commonly used with Collections class ArrayList than array, less chance of OutOfBounds exceptions */
         for (int iterator : grades) System.out.print(iterator + " ");
+
+        /* remove all instances of a specific element from the ArrayList */
+        // grades.removeAll(Collections.singleton(90));
+
+        /* Implementation of Java 8 Streams;
+        Line grabs grades ArrayList, converts it into a stream, applies filter that excludes all instances of element 90,
+        collect gives back grades from stream to ArrayList;
+        Note: {} can be included within stream functions to allow for subfunctions to be run, enabling more complexity */
+        grades=grades.stream().filter(i-> i!=90).collect(Collectors.toList());
+        System.out.print("\n Without grades of 90: ");
+        for (int iterator : grades) System.out.print(iterator + " ");
     }
 }
-// compile with javac Array_List.java --release 8, run with java Array_List
+// compile with javac Array_List.java --release 8 (bc unmatching compile/run java versions), run with java Array_List
